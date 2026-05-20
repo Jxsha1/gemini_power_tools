4beda7bee030d9da330631515ea8966d227ff449
Jxsha1 <joshstevens10697@gmail.com>
2026-05-21 00:32:51 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,12 @@ export default defineConfig({
   vite: {
     build: {
       rollupOptions: {
+        input: {
+          sidebar: 'src/pages/sidebar.astro'
+        },
         output: {
+          entryFileNames: 'assets/[name].js',
+          chunkFileNames: 'assets/[name].js',
           assetFileNames: (assetInfo) => {
             if (assetInfo.name && assetInfo.name.endsWith('.css')) {
               return 'assets/sidebar.[ext]';
