e6e004721d71cfb3e5aa3387f458c8d9c99326c1
Jxsha1 <joshstevens10697@gmail.com>
2026-05-21 00:03:23 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,19 @@ export default defineConfig({
   output: 'static',
   build: {
     assets: 'assets'
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+              return 'assets/sidebar.[ext]';
+            }
+            return 'assets/[name].[ext]';
+          }
+        }
+      }
+    }
   }
 });
